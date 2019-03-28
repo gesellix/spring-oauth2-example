5dab58da83e89e08d567f721d51f57fd89f17d87
Tobias Gesellchen <tobias@gesellix.de>
2019-03-28 13:43:39 +0100

    prepare different variants

--- creds-example-client/src/main/groovy/com/example/credsexampleclient/ClientApplication.groovy
+++ creds-example-client/src/main/groovy/com/example/credsexampleclient/ClientApplication.groovy
@@ -1,6 +1,7 @@
 package com.example.credsexampleclient
 
 import groovy.util.logging.Slf4j
+import org.apache.commons.codec.Charsets
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
@@ -10,6 +11,8 @@ import org.springframework.context.annotation.Bean
 import org.springframework.security.oauth2.client.OAuth2RestTemplate
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails
 import org.springframework.web.client.RestTemplate
+
+import java.nio.charset.Charset
 
 @Slf4j
 @SpringBootApplication
@@ -35,7 +38,25 @@ class ClientApplication implements CommandLineRunner {
 
   @Override
   void run(String... args) {
-    String response = restTemplate().getForObject(serverBaseUrl + "/mod", String)
-    log.info("MOD: {}", response)
+
+    String mode = System.getenv("MODE") ?: "DEMO"
+
+    String response
+
+    switch (mode) {
+      case "DEMO":
+        response = restTemplate().getForObject(serverBaseUrl + "/mod", String)
+        break
+      default:
+        response = restTemplate().getForObject(serverBaseUrl + "/mod", String)
+        break
+    }
+    log.info("Response: {}", response)
+  }
+
+  private String toString(InputStream inputStream, Charset charset = Charsets.UTF_8) throws IOException {
+
+    Scanner scanner = new Scanner(inputStream, charset.name())
+    return scanner.useDelimiter("\\A").next()
   }
 }
